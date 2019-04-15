d37f7aa6a64089adc45af0116859e7a3cd692c07
Ivan Karlen <Ivannicolaskarlen@hotmail.com>
2019-04-15 12:36:31 -0300

    Hemos agregado los archivos

---
+++ ConComandos/InvertirArray.php
@@ -0,0 +1,18 @@
+<?php
+$pepe = array('Gato','Perro','Caballo','Pez','Tomate');
+
+$reversa = array_reverse($pepe);
+$variable = array_reverse($pepe, true); //Si se establece en TRUE las claves numéricas se conservarán.
+// Las claves no numéricas no se ven afectadas por este ajuste y se conservarán siempre
+
+print_r("Ordenados");
+print_r("<br>");print_r("<br>");
+print_r($pepe);
+print_r("<br>");print_r("<br>");
+print_r("Inversa");
+print_r("<br>");print_r("<br>");
+print_r($reversa);
+
+
+
+?>
